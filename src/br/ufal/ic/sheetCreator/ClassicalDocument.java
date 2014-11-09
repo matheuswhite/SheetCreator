bcacae6ce5d769ee951717cee9cbe5f453ca91b3
Matheus Tenorio <matheustenorio@ic.com.br>
2014-11-09 20:52:55 -0300

    Add kind of documents

---
+++ src/br/ufal/ic/sheetCreator/ClassicalDocument.java
@@ -0,0 +1,21 @@
+package br.ufal.ic.sheetCreator;
+
+import java.util.ArrayList;
+
+import br.ufal.ic.sheetCreator.decorator.MusicalStaff;
+
+public class ClassicalDocument extends Document {
+
+	public ClassicalDocument(String title, String author, ArrayList<MusicalStaff> staffs) {
+		super(title, author, staffs);
+	}
+
+	@Override
+	public void setStaffs(ArrayList<MusicalStaff> staffs) {
+		this.add(staffs.get(0).getLabel());
+		this.add(staffs.get(1).getLabel());
+		this.add(staffs.get(2).getLabel());
+		this.add(staffs.get(3).getLabel());
+	}
+
+}
--- src/br/ufal/ic/sheetCreator/Document.java
+++ src/br/ufal/ic/sheetCreator/Document.java
@@ -1,5 +1,51 @@
 package br.ufal.ic.sheetCreator;
 
-public class Document {
+import java.util.ArrayList;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import br.ufal.ic.sheetCreator.decorator.MusicalStaff;
+
+public abstract class Document extends JPanel implements IDocument {
+	
+	private int[] position;
+	private JLabel title;
+	private JLabel author;
+	private JTextArea editTitle;
+	
+	private JLabel firstStaff;
+	private JLabel secondStaff;
+	private JLabel thirdStaff;
+	private JLabel fourthStaff;
+	
+	public Document(String title, String author, ArrayList<MusicalStaff> staffs) {
+		this.setLayout(null);
+		this.position = new int[1];
+		
+		this.setTitle(title);
+		this.setAutor(author);
+		this.setStaffs(staffs);
+	}
+	
+	public abstract void setStaffs(ArrayList<MusicalStaff> staffs);
+	
+	public void setTitle(String title) {
+		if(title == null) {
+			this.title = new JLabel("Sem Título");
+		}
+		else {
+			this.title = new JLabel(title);
+		}
+	}
+	
+	public void setAutor(String author) {
+		if(author == null) {
+			this.author = new JLabel("Sem Título");
+		}
+		else {
+			this.author = new JLabel(author);
+		}
+	}
 }
